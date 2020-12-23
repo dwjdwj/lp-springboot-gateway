076b4ca28c5cd54ded267176a333895513babf32
[dwjdwj] <[qq376337326@qq.com]>
2020-12-23 11:07:50 +0800

    超时设置

---
+++ lp-gateway/src/main/java/com/cn/lp/qrcard/gateway/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.cn.lp.qrcard.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+
+    @RequestMapping("/fallback")
+    public String timeout(){
+        try{
+            //睡5秒，网关Hystrix3秒超时，会触发熔断降级操作
+            Thread.sleep(2000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "timeout";
+    }
+}
